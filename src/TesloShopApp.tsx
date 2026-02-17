26bc6bda859a0183a5cafd0ff3f8f2ffef429ca9
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-02-17 08:03:14 -0500

    instalacion de devtools de tanstack y configuracion en TesloShopApp.tsx

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -1,12 +1,22 @@
 import { RouterProvider } from "react-router-dom";
 import { AppRouter } from "./app.router";
 import { Toaster } from "@/components/ui/sonner";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const TesloShopApp = () => {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <RouterProvider router={AppRouter} />
-      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        {/* The rest of your application */}
+
+        <RouterProvider router={AppRouter} />
+        <Toaster />
+
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 };
