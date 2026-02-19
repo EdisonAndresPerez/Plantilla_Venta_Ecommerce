1a39fca09511ecdd0851d7c343b4c2a398a3851b
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-02-19 13:45:33 -0500

    paginacion terminada con logica

--- src/shop_front/hooks/useProducts.tsx
+++ src/shop_front/hooks/useProducts.tsx
@@ -1,9 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductsAction } from "../actions/get-products-action";
+import { useSearchParams } from "react-router";
 
 export const useProducts = () => {
+  const [searchParams] = useSearchParams();
+  const limit = searchParams.get("limit") || 9;
+  const page = searchParams.get("page") || 0;
+
+  const offset = page ? (Number(page) - 1) * Number(limit) : 0;
+
   return useQuery({
-    queryKey: ["products"],
-    queryFn: getProductsAction,
+    queryKey: ["products", { limit, offset }],
+    queryFn: () =>
+      getProductsAction({
+        limit: limit,
+        offset: offset,
+      }),
   });
 };
