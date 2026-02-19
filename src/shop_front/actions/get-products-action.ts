9fc8386e951b717809876ce5a01036ea859d3f6a
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-02-19 11:28:45 -0500

    arreglar las imagenes de los productos

--- src/shop_front/actions/get-products-action.ts
+++ src/shop_front/actions/get-products-action.ts
@@ -1,11 +1,19 @@
 import { tesloApi } from "@/api/tesloApi";
 import type { Products } from "@/interfaces/products.response";
 
-
-
 export const getProductsAction = async () => {
   const { data } = await tesloApi.get<Products>("/products");
-  console.log(data)
+  console.log(data);
 
-  return data;
+  const productsWithImagesUrl = data.products.map((product) => ({
+    ...product,
+    images: product.images.map(
+      (image) => `${import.meta.env.VITE_API_URL}/files/product/${image}`,
+    ),
+  }));
+
+  return {
+    ...data,
+    products: productsWithImagesUrl,
+  }
 };
